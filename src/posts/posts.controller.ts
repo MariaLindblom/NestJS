75195d3f432cdca2992b15d0fe75179af6ca20d6
marialindblom <ml6581@edu.turku.fi>
2022-11-10 13:44:28 +0100

    added ApiResponse and imports

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Param, ParseIntPipe, Post, Body, Delete, Put } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostModel } from './posts/posts.interface';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOkResponse, ApiNotFoundResponse, ApiCreatedResponse, ApiUnprocessableEntityResponse } from '@nestjs/swagger';
 
 @Controller('posts')
 @ApiTags('posts')
@@ -9,26 +9,36 @@ export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
+  @ApiOkResponse({ description: 'Posts retrieved successfully.' })
   public findAll(): Array<PostModel> {
     return this.postsService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ description: 'Post retrieved successfully.' })
+  @ApiNotFoundResponse({ description: 'Post not found.' })
   public findOne(@Param('id', ParseIntPipe) id: number): PostModel {
     return this.postsService.findOne(id);
   }
 
   @Post()
+  @ApiCreatedResponse({ description: 'Post created successfully.' })
+  @ApiUnprocessableEntityResponse({ description: 'Post title already exists.' })
   public create(@Body() post: PostModel): PostModel {
     return this.postsService.create(post);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ description: 'Post deleted successfully.' })
+  @ApiNotFoundResponse({ description: 'Post not found.' })
   public delete(@Param('id', ParseIntPipe) id: number): void {
     this.postsService.delete(id);
   }
 
   @Put(':id')
+  @ApiOkResponse({ description: 'Post updated successfully.' })
+  @ApiNotFoundResponse({ description: 'Post not found.' })
+  @ApiUnprocessableEntityResponse({ description: 'Post title already exists.' })
   public update(@Param('id', ParseIntPipe) id: number, @Body() post: PostModel): PostModel {
     return this.postsService.update(id, post);
   }
