ebf4de2a322ce17a301c6efef6b0418d3f14a17a
Thoumey <thomas.borgelin@gmail.com>
2022-11-14 10:50:39 +0200

    include postsservice in tests

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
 describe('PostsController', () => {
   let controller: PostsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [PostsService],
       controllers: [PostsController],
     }).compile();
 
