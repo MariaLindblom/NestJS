e10022206f6a29a3cbdf05de0dbc6b3af2383195
Thoumey <thomas.borgelin@gmail.com>
2022-11-17 15:22:49 +0200

    typeorm integration + hide sensitive data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import dbconfig from "dbconfig.json";
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: dbconfig.host,
+      port: dbconfig.port,
+      username: dbconfig.username,
+      password: dbconfig.password,
+      database: dbconfig.database,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
