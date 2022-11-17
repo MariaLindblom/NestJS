5f597a431a8f943079669efea68a4923d0968cc9
marialindblom <ml6581@edu.turku.fi>
2022-11-17 15:33:30 +0200

    changing app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'harjoitus.mysql.database.azure.com:3306',
+      port: 3306,
+      username: 'empirica',
+      password: '',
+      database: 'harkka',
+      entities: [],
+      synchronize: false,
+    }),
+    PostsModule
+  ],
   controllers: [],
   providers: [],
 })
