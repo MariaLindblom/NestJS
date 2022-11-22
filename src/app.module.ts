6176af72ec91063af71448cf0aa487deb2e0ed0d
marialindblom <ml6581@edu.turku.fi>
2022-11-22 15:36:41 +0200

    Undefined error fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
-import dbconfig from "dbconfig.json";
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import * as dbconfig from "dbconfig.json";
 
 @Module({
   imports: [
