48411acd40f1b7a728df755246b64942d958d5df
marialindblom <ml6581@edu.turku.fi>
2022-11-30 14:06:59 +0200

    data from the database table can be modified from the api(?)

--- src/posts/posts.queries.ts
+++ src/posts/posts.queries.ts
@@ -1,3 +1,16 @@
+import { create } from "domain";
+import "reflect-metadata";
+import { Posts } from './posts.entity';
+import { PostsService } from "./posts.service";
+
+export const post = new Posts()
+post.post_date = new Date (2022-11-29)
+post.title = "Testi"
+post.content = "Testausta"
+post.category = "Test"
+//await postrepositary.save()
+
+create
 export const PostQueries = {
   AddPost: `
   INSERT INTO harkka.posts (post_date, title, content, category) VALUES(20221129, "Testi", "Testausta", "Test")`
